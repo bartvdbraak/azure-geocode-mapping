cf1a46053f4b4bd1cebcc6863a9c0f528c31c15d
Bart van der Braak <bartvdbraak@gmail.com>
2023-04-26 18:15:48 +0200

    Added function to retrieve role definition data

--- data.sh
+++ data.sh
@@ -5,6 +5,7 @@ readonly BASEDIR="$(cd "$(dirname "$0")" && pwd)"
 readonly REGIONS_OUTPUT_FILE="$BASEDIR/data/azure-regions.json"
 readonly GEOCODES_OUTPUT_FILE="$BASEDIR/data/azure-geocodes.xml"
 readonly GEOCODES_SOURCE_URL="https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/backup/scripts/geo-code-list.md"
+readonly ROLE_DEFINITIONS_OUTPUT_FILE="$BASEDIR/data/azure-role_definitions.json"
 
 # Check if Azure CLI is installed
 check_azure_cli() {
@@ -41,12 +42,18 @@ get_azure_geocodes() {
   printf "%s" "$filtered_content" > "$GEOCODES_OUTPUT_FILE"
 }
 
+# Get azure regions and save to azure-regions.json
+get_azure_role_definitions() {
+  az role definition list > "$ROLE_DEFINITIONS_OUTPUT_FILE"
+}
+
 # Main function
 main() {
   check_azure_cli
   check_active_account
   get_azure_regions
   get_azure_geocodes
+  get_azure_role_definitions
 }
 
 # Call the main function
